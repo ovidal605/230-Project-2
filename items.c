a07df00b58eb945bf3622dfaf6acf0a08c5ec151
ovidal605 <ovidal605@gmail.com>
2019-02-22 16:40:15 -0500

    moved Item _take function

--- items.c
+++ items.c
@@ -1,1 +1,24 @@
 #include "items.h"
+
+// Removes an item from the list with a matching name itemName
+struct Item *item_take(struct Item *item, char *itemName)
+{
+  // Get a referance to the next item in the list
+  struct Item *next = item_next(item);
+
+  // While there is a next item
+  while (next != NULL)
+  {
+
+    // If found, remove it from the list
+    if (strcmp(item_name(next), itemName) == 0)
+    {
+      item->next = item_next(next);
+      return item;
+    }
+
+    // progress the current and next item by one index
+    item = item_next(item);
+    next = item_next(next);
+  }
+}
--- main.c
+++ main.c
@@ -79,28 +79,7 @@ struct Item *item_next(struct Item *item)
 {
   return item->next;
 }
-// Removes an item from the list with a matching name itemName
-struct Item *item_take(struct Item *item, char *itemName)
-{
-  // Get a referance to the next item in the list
-  struct Item *next = item_next(item);
 
-  // While there is a next item
-  while (next != NULL)
-  {
-
-    // If found, remove it from the list
-    if (strcmp(item_name(next), itemName) == 0)
-    {
-      item->next = item_next(next);
-      return item;
-    }
-
-    // progress the current and next item by one index
-    item = item_next(item);
-    next = item_next(next);
-  }
-}
 //prints the item list
 void item_print(struct Item *head)
 {
